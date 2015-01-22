748c45866d65b208c9cc05064167527b8684c371
Justin Phelps <linuturk@onitato.com>
2015-01-22 13:07:29 -0600

    first shot at a Railo template

---
+++ test/fabric/railo_server.py
@@ -0,0 +1,28 @@
+# coding=utf8
+
+import re
+
+from fabric.api import env, hide, run, task
+from envassert import detect, file, package, port, process, service
+
+
+def apache_is_responding():
+    with hide('running', 'stdout'):
+        site = "https://localhost/"
+        homepage = run("wget --quiet --output-document - --no-check-certificate %s" % site)
+        if re.search('Apache2 Ubuntu Default Page', homepage):
+            return True
+        else:
+            return False
+
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert file.exists("/opt/railo/install.log"), "Railo install log missing."
+    assert port.is_listening(80), "Port 80 is not listening."
+    assert process.is_up("java"), "The java process is not running."
+    assert process.is_up("apache2"), "The apache2 process is not running."
+    assert service.is_enabled("apache2"), "The apache2 service is not enabled."
+    assert apache_is_responding(), "Apache is not responding."
